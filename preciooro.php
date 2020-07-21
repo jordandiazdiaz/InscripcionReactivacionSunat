3d463827bfd6a5bae8829eeb19af9b45de7cf740
Jordan Diaz <jordandiaz2016@gmail.com>
2020-07-21 14:14:20 -0500

    ok

--- preciooro.php
+++ preciooro.php
@@ -11,21 +11,11 @@ $xpath = new DomXPath($dom);
 $nodes = $xpath->query('//div[3]/div[3]/div[1]/div[1]/div[1]/div[2]/table/tr[last()]/td[1]');
 if ($nodes->length) {
     //Imprime la AM
-<<<<<<< HEAD
-    echo $nodes[0]->textContent;
-}    
-echo "---";
-$nodes = $xpath->query('//div[3]/div[3]/div[1]/div[1]/div[1]/div[2]/table/tr[last()]/td[2]');
-if ($nodes->length) {
-    //Imprime la PM
-    echo $nodes[0]->textContent;
-=======
     $am = $nodes[0]->textContent;
 }    
 $nodes = $xpath->query('//div[3]/div[3]/div[1]/div[1]/div[1]/div[2]/table/tr[last()]/td[2]');
 if ($nodes->length) {
     //Imprime la PM
     $pm = $nodes[0]->textContent;
->>>>>>> 0f98030231d97706329f86ba75da5339d68ece54
 }    
 ?>
--- tipodecambio.php
+++ tipodecambio.php
@@ -11,19 +11,10 @@ $xpath = new DomXPath($dom);
 $nodes = $xpath->query('//div[2]/center/table/tr[last()]/td[last()]');
 if ($nodes->length) {
     //Imprime la venta
-<<<<<<< HEAD
-    echo $nodes[0]->textContent;
-=======
     $precio_compra =  $nodes[0]->textContent;
->>>>>>> 0f98030231d97706329f86ba75da5339d68ece54
 }    
 $nodes = $xpath->query('//div[2]/center/table/tr[last()]/td[last()-1]');
 if ($nodes->length) {
     //Imprime la compra
-<<<<<<< HEAD
-    echo $nodes[0]->textContent;
-=======
     $precio_venta = $nodes[0]->textContent;
->>>>>>> 0f98030231d97706329f86ba75da5339d68ece54
 }    
-?>
